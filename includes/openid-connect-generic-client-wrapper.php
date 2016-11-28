e7f390477c13cc3ee769ea83049f5d7675c748e2
Jonathan Daggerhart <jonathan@daggerhart.com>
2016-11-28 11:04:09 -0500

    fixing error on php 5.4

--- includes/openid-connect-generic-client-wrapper.php
+++ includes/openid-connect-generic-client-wrapper.php
@@ -387,7 +387,9 @@ class OpenID_Connect_Generic_Client_Wrapper {
 		$this->logger->log( "Successful login for: {$user->user_login} ({$user->ID})", 'login-success' );
 
 		// redirect back to the origin page if enabled
-		if( $this->settings->redirect_user_back && !empty( $redirect_url = esc_url( $_COOKIE[ $this->cookie_redirect_key ] ) ) ) {
+		$redirect_url = esc_url( $_COOKIE[ $this->cookie_redirect_key ] );
+
+		if( $this->settings->redirect_user_back && !empty( $redirect_url ) ) {
 			do_action( 'openid-connect-generic-redirect-user-back', $redirect_url, $user );
 			setcookie( $this->cookie_redirect_key, $redirect_url, 1, COOKIEPATH, COOKIE_DOMAIN, is_ssl() );
 			wp_redirect( $redirect_url );
