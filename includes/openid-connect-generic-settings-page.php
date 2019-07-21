5863d78dd06806f9fcec18eb1205afe0bf85c189
benoit <benoit.chenal.57@gmail.com>
2019-07-21 21:18:49 +0200

    Add settings which failed the authentication if the user's identity cannot be mapped to an existing wordpress user

--- includes/openid-connect-generic-settings-page.php
+++ includes/openid-connect-generic-settings-page.php
@@ -167,6 +167,12 @@ class OpenID_Connect_Generic_Settings_Page {
 				'type'        => 'checkbox',
 				'section'     => 'user_settings',
 			),
+			'fail_not_map_existing_wordpress_user'   => array(
+				'title'       => __( 'User shall exist' ),
+				'description' => __( 'If the user\'s identity is not linked to an existing Wordpress user. The authentication failed.' ),
+				'type'        => 'checkbox',
+				'section'     => 'user_settings',
+			),
 			'redirect_user_back'   => array(
 				'title'       => __( 'Redirect Back to Origin Page' ),
 				'description' => __( 'After a successful OpenID Connect authentication, this will redirect the user back to the page on which they clicked the OpenID Connect login button. This will cause the login process to proceed in a traditional WordPress fashion. For example, users logging in through the default wp-login.php page would end up on the WordPress Dashboard and users logging in through the WooCommerce "My Account" page would end up on their account page.' ),
