4fbe3dff195ad059361ac2d980868050c77ecd9e
Clement Boirie <cboirie@beapi.fr>
2022-07-11 15:04:02 +0200

    Add new PKCE setting
    Add new setting to enable/disable PKCE feature. A new constant
    OIDC_ENABLE_PKCE is available to force the setting's value.

--- openid-connect-generic.php
+++ openid-connect-generic.php
@@ -375,6 +375,7 @@ class OpenID_Connect_Generic {
 				'endpoint_token'       => defined( 'OIDC_ENDPOINT_TOKEN_URL' ) ? OIDC_ENDPOINT_TOKEN_URL : '',
 				'endpoint_end_session' => defined( 'OIDC_ENDPOINT_LOGOUT_URL' ) ? OIDC_ENDPOINT_LOGOUT_URL : '',
 				'acr_values'           => defined( 'OIDC_ACR_VALUES' ) ? OIDC_ACR_VALUES : '',
+				'enable_pkce'          => defined( 'OIDC_ENABLE_PKCE' ) ? OIDC_ENABLE_PKCE : false,
 
 				// Non-standard settings.
 				'no_sslverify'           => 0,
